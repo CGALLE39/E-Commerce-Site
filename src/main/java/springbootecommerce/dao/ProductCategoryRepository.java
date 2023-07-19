0c8883f36b66291dcd2a21635a5c06f93d56ac2c
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-07-19 10:55:29 -0500

    table for products

--- src/main/java/springbootecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/springbootecommerce/dao/ProductCategoryRepository.java
@@ -1,7 +1,10 @@
 package springbootecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import springbootecommerce.entity.ProductCategory;
+
+@CrossOrigin("http://localhost:4200")
 
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- src/main/java/springbootecommerce/dao/ProductRepository.java
+++ src/main/java/springbootecommerce/dao/ProductRepository.java
@@ -3,8 +3,9 @@ package springbootecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import springbootecommerce.entity.Product;
-
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
